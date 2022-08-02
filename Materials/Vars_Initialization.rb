e4bed7d4439198e8c26b8bb11e956cea428deceb
stesproject <stefmercadante@gmail.com>
2022-08-02 09:29:55 +0200

    update

--- Materials/Vars_Initialization.rb
+++ Materials/Vars_Initialization.rb
@@ -8,6 +8,7 @@ class Vars_Initialization
     $game_switches[12] = true #save vittoria
     $game_switches[13] = true #save menu
     $game_switches[217] = false #quit game
+    $game_variables[62] = 2 #Trasporto
 
     # Debug
     $game_switches[556] = $TEST && false #attack-up
